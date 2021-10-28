0293a7035458301056d81331b515d198db2b70b9
Robert Rodford <rbenrodford@gmail.com>
2021-10-28 15:07:39 -0400

    french demos playlist

--- js/player.js
+++ js/player.js
@@ -1,3 +1,6 @@
+
+const demoPath = '/home/ben/Stuff/Prog/Gregory Website/www/demo/'
+
 jQuery(function ($) {
     'use strict'
     var supportsAudio = !!document.createElement('audio').canPlayType;
@@ -19,58 +22,75 @@ jQuery(function ($) {
             playing = false,
             mediaPath = demoPath,
             extension = '',
-            tracks = [{
+            tracksEN = [{
                 "track": 1,
-                "name": "Commercial Voice Demo",
-                "duration": "0:58",
-                "file": "Gregory Walle Commercial Voice Demo v3 AUG 11"
-            }, {
-                "track": 2,
                 "name": "LL BEAN",
                 "duration": "0:07",
                 "file": "Gregory Walle LL BEAN"
             }, {
-                "track": 3,
+                "track": 2,
                 "name": "McCafe",
                 "duration": "0:10",
                 "file": "Gregory Walle McCafe"
             }, {
-                "track": 4,
+                "track": 3,
                 "name": "McCafe v2",
                 "duration": "0:12",
                 "file": "Gregory Walle McCafe v2"
             }, {
-                "track": 5,
+                "track": 4,
                 "name": "Morocco",
                 "duration": "0:09",
                 "file": "Gregory Walle Morocco"
             }, {
-                "track": 6,
+                "track": 5,
                 "name": "Molson Canadian",
                 "duration": "0:09",
                 "file": "Gregory Walle Molson Canadian"
             }, {
-                "track": 7,
+                "track": 6,
                 "name": "Scotia Bank",
                 "duration": "0:07",
                 "file": "Gregory Walle Scotia Bank"
             }, {
-                "track": 8,
+                "track": 7,
                 "name": "Denver Broncos and Coffee",
                 "duration": "0:09",
                 "file": "Gregory Walle Denver Broncos and Coffee"
             }, {
-                "track": 9,
+                "track": 8,
                 "name": "Mazda",
                 "duration": "0:13",
                 "file": "Gregory Walle Mazda"
             }, {
-                "track": 10,
+                "track": 9,
                 "name": "Kansas Power",
                 "duration": "0:12",
                 "file": "Gregory Walle Kansas Power"
             }],
-            buildPlaylist = $.each(tracks, function(key, value) {
+            tracksFR = [{
+                "track": 1,
+                "name": "Alexa",
+                "duration": "0:22",
+                "file": "ALEXA FR"
+            }, {
+                "track": 2,
+                "name": "Imperturbable",
+                "duration": "0:59",
+                "file": "IMPERTURBABLE FR"
+            }, {
+                "track": 3,
+                "name": "Manuvie",
+                "duration": "0:30",
+                "file": "Manuvie Fr"
+            }, {
+                "track": 4,
+                "name": "Murale",
+                "duration": "0:39",
+                "file": "murale fr"
+            }],
+            tracks = tracksEN.concat(tracksFR),
+            buildPlaylistEN = $.each(tracksEN, function(key, value) {
                 var trackNumber = value.track,
                     trackName = value.name,
                     trackDuration = value.duration;
@@ -78,7 +98,23 @@ jQuery(function ($) {
                     trackNumber = '0' + trackNumber;
                 }
                 // create HTML playlist section
-                $('#plList').append('<li> \
+                $('#plEn').append('<li> \
+                    <div class="plItem"> \
+                        <span class="plNum">' + trackNumber + '.</span> \
+                        <span class="plTitle">' + trackName + '</span> \
+                        <span class="plLength">' + trackDuration + '</span> \
+                    </div> \
+                </li>');
+            }),
+            buildPlaylistFR = $.each(tracksFR, function(key, value) {
+                var trackNumber = value.track,
+                    trackName = value.name,
+                    trackDuration = value.duration;
+                if (trackNumber.toString().length === 1) {
+                    trackNumber = '0' + trackNumber;
+                }
+                // create HTML playlist section
+                $('#plFr').append('<li> \
                     <div class="plItem"> \
                         <span class="plNum">' + trackNumber + '.</span> \
                         <span class="plTitle">' + trackName + '</span> \
@@ -133,15 +169,18 @@ jQuery(function ($) {
                     loadTrack(index);
                 }
             }),
-            li = $('#plList li').on('click', function () {
+            li = $('#plEn li').on('click', function () {
                 var id = parseInt($(this).index());
-                if (id !== index) {
-                    playTrack(id);
-                }
+                clickTrack(id);
+            }),
+            li = $('#plFr li').on('click', function () {
+                var id = parseInt($(this).index());
+                id += tracksEN.length;
+                clickTrack(id);
             }),
             loadTrack = function (id) {
                 $('.plSel').removeClass('plSel');
-                $('#plList li:eq(' + id + ')').addClass('plSel');
+                $('.plList li:eq(' + id + ')').addClass('plSel');
                 npTitle.text(tracks[id].name);
                 index = id;
                 audio.src = mediaPath + tracks[id].file + extension;
@@ -151,6 +190,19 @@ jQuery(function ($) {
                 player.on('loadedmetadata', function () {
                     $('a[data-plyr="download"]').attr('href', source);
                 });
+            },
+            clickTrack = function (id) {
+                if (id !== index) {
+                    playTrack(id);
+                } else {
+                    if (playing) {
+                        audio.pause();
+                        playing = false;
+                    } else {
+                        audio.play();
+                        playing = true;
+                    }
+                }
             },
             playTrack = function (id) {
                 loadTrack(id);
--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,6 @@
 // Scripts
 // 
 
-const demoPath = '/demo/'
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
@@ -74,50 +72,5 @@ jQuery(function ($) {
         $('[lang="fr"]').hide();
         $.cookie('lang', 'en', { expires: 7 });
         langButtonListen();
-
-
-    //   if ("geolocation" in navigator) {
-    //     // geolocation is available
-    //     navigator.geolocation.getCurrentPosition(function (position) {
-    //       // accepted geolocation so figure out which country
-    //       var lat = position.coords.latitude,
-    //           lng = position.coords.longitude;
-    //       $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&sensor=true', null, function (response) {
-    //         var country = response.results[response.results.length-1].formatted_address;
-    //         if (country ===  'France' || country === 'China') {
-    //           $('[lang="en"]').hide();
-    //           $.cookie('lang', 'fr', { expires: 7 });
-    //           langButtonListen();
-    //         } else {
-    //           $('[lang="fr"]').hide();
-    //           $.cookie('lang', 'en', { expires: 7 });
-    //           langButtonListen();
-    //         }
-    //       }).fail(function (err) {
-    //         console.log('error: '+err);
-    //         $('[lang="fr"]').hide();
-    //         $.cookie('lang', 'en', { expires: 7 });
-    //         langButtonListen();
-    //       });
-    //     },
-    //     function (error) {
-    //       if (error.code == error.PERMISSION_DENIED) {
-    //         // denied geolocation
-    //         $('[lang="fr"]').hide();
-    //         $.cookie('lang', 'en', { expires: 7 });
-    //         langButtonListen();
-    //       } else {
-    //         console.log('Unknown error. Defaulting to English!');
-    //         $('[lang="fr"]').hide();
-    //         $.cookie('lang', 'en', { expires: 7 });
-    //         langButtonListen();
-    //       }
-    //     });
-    //   } else {
-    //     // geolocation IS NOT available
-    //     $('[lang="fr"]').hide();
-    //     $.cookie('lang', 'en', { expires: 7 });
-    //     langButtonListen();
-    //   }
     }
   });
